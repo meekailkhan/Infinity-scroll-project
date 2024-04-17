15d49d1be5ce1dc53cec958f009742809bed1303
meekailkhan <meekailkhan123aar@gmail.com>
2024-04-17 23:27:11 +0530

    this is infinity scroll project

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 const body = document.body
 const maincontainer = document.querySelector(".mainContainer")
 
+function cardGenretor(tag,attName,attType,parent){
+    let element = document.createElement(tag);
+    if(!! attName && attType){
+        element.setAttribute(attName,attType)
+    }
+    if(!!parent){
+        parent.append(element)
+    }
+    return element;
+}
 
 window.addEventListener("scrollend",function(e){
-    console.log(e)
+    let fragment = document.createDocumentFragment()
+    for(let i =0;i<15;i++){
+        cardGenretor("div","class","card",maincontainer)
+    }
 })
