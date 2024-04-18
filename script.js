ef66ffcfcde43f32c6aa2c72c2b038c2592b2cf0
meekailkhan <meekailkhan123aar@gmail.com>
2024-04-18 18:25:13 +0530

    this is infinity scroll project

--- script.js
+++ script.js
@@ -1,20 +1,51 @@
-const body = document.body
-const maincontainer = document.querySelector(".mainContainer")
+// // const body = document.body
+// // const maincontainer = document.querySelector(".mainContainer")
 
-function cardGenretor(tag,attName,attType,parent){
-    let element = document.createElement(tag);
-    if(!! attName && attType){
-        element.setAttribute(attName,attType)
-    }
-    if(!!parent){
-        parent.append(element)
-    }
-    return element;
-}
+// // function cardGenretor(tag,attName,attType,parent){
+// //     let element = document.createElement(tag);
+// //     if(!! attName && attType){
+// //         element.setAttribute(attName,attType)
+// //     }
+// //     if(!!parent){
+// //         parent.append(element)
+// //     }
+// //     return element;
+// // }
 
-window.addEventListener("scrollend",function(e){
-    let fragment = document.createDocumentFragment()
-    for(let i =0;i<15;i++){
-        cardGenretor("div","class","card",maincontainer)
-    }
-})
+// // window.addEventListener("scrollend",function(e){
+// //     for(let i =0;i<15;i++){
+// //         cardGenretor("div","class","card",maincontainer)
+// //     }
+// // })
+
+// const body = document.body;
+// const maincontainer = document.querySelector(".mainContainer");
+
+// function cardGenerator(tag, attName, attType, parent) {
+//     let element = document.createElement(tag);
+//     if (!!attName && attType) {
+//         element.setAttribute(attName, attType);
+//     }
+//     if (!!parent) {
+//         parent.append(element);
+//     }
+//     return element;
+// }
+
+// window.addEventListener("scrollend", function (e) {
+//     let fragment = document.createDocumentFragment();
+
+//     for (let i = 0; i < 15; i++) {
+//         let card = cardGenerator("div", "class", "card", null);
+//         fragment.appendChild(card);
+//     }
+
+//     maincontainer.appendChild(fragment);
+// });
+
+const body = document.body;
+const maincontainer = document.querySelector(".mainContainer");
+
+window.addEventListener("scrollend", function (e) {
+    maincontainer.innerHTML +=  "<div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>"
+});
